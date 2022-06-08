69794670e420d24c19495751e7a1965dacf16e66
Peter Baltov <p.baltov.19@abdn.ac.uk>
2022-06-08 23:01:31 +0300

    Domain + HTTPS added

--- src/pages/App.js
+++ src/pages/App.js
@@ -109,12 +109,12 @@ class Home extends React.Component {
       let headers = new Headers();
       headers.append('Content-Type', 'application/json' );
       headers.append('Accept', 'application/json');
-      headers.append('Origin','https://89.215.222.118:443/upload');
+      headers.append('Origin','https://cosednetwork.com/upload');
       this.setState({loading: true})
       console.log('Making post request');
       // Change this location once the server-side implementation has
       // been deployed to a remote or cloud service provider
-      fetch('https://89.215.222.118:443/upload', {
+      fetch('https://cosednetwork.com/upload', {
           method: 'POST',
           headers: headers,
           body: JSON.stringify({ img: this.state.base64 }),
